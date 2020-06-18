f7f5e7e49410aa4a6348ece645a97999ac08b95d
jainmo2 <monil.jain@navis.com>
2020-06-18 04:03:58 -0700

    init

---
+++ src/main/java/com/bart/Bart/RestController.java
@@ -0,0 +1,18 @@
+package com.bart.Bart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+//    @ResponseBody
+//    @GetMapping("/stations")
+//    public StationList getAllStations(){
+//        RestTemplate restTemplate=new RestTemplate();
+//        restTemplate.getForObject("http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y",)
+//    }
+
+}
