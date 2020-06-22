f00726714f101bcb10a365c6d770fa63ceb1702d
jainmo2 <monil.jain@navis.com>
2020-06-21 20:51:25 -0700

    integrated front end and backend properly

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -6,3 +6,8 @@ function getStationInfo() {
     var source = document.getElementById("station").value;
     window.location.replace("http://localhost:8080/station/"+source);
 }
+function getAllTrains() {
+    var source = document.getElementById("from").value;
+    var dest = document.getElementById("to").value;
+    window.location.replace("http://localhost:8080/"+source+"/"+dest);
+}
